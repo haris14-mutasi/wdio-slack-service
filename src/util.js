7a55085b302f1aba25431c2af6919fdef885fdf0
Harith Fanani <harith.fanani@gmail.com>
2022-06-08 20:06:39 +0700

    Add description

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 const failedAttachment = function(test, errorMessage, { duration }) {
     const failedMessage = {
       color: `#dc3545`,
-      author_name: `${test.title}`,
+      author_name: `${test.title || test.description}`,
       footer: `Uh! Oh! FAILED -:stopwatch: ${timeConverter(duration)}\n${test._currentRetry ? `This is a retried test and failed after ${test._currentRetry > 1 ? `${test._currentRetry} retries` : `${test._currentRetry} retry`}` : ``}\n${errorMessage}`,
       footer_icon: `https://www.pinclipart.com/picdir/big/31-316209_circle-x-clipart-reject-icon-png-download.png`,
       ts: Date.now()
@@ -12,7 +12,7 @@ const failedAttachment = function(test, errorMessage, { duration }) {
 const passedAttachment = function(test, { duration }) {
     const passedMessage = {
         color: `#6bc77c`,
-        author_name: `${test.title}`,
+        author_name: `${test.title || test.description}`,
         footer: `Woo! Ooh! PASSED - :stopwatch: ${timeConverter(duration)}\n${test._currentRetry ? `This is a retried test and passed after ${test._currentRetry > 1 ? `${test._currentRetry} retries` : `${test._currentRetry} retry`}` : ``}`,
         footer_icon: `https://vectorified.com/images/icon-pass-19.png`
     };
