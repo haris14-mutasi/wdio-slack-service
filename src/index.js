43e4d0119055095f405a0434da3e1ee93aabd330
Harith Fanani <harith.fanani@gmail.com>
2022-06-12 17:06:38 +0700

    Remove unused flag

--- src/index.js
+++ src/index.js
@@ -45,69 +45,26 @@ class SlackService {
                 ++this.failedTests;
                 ++this.tests;
                 const attach = failedAttachment(test, testError.toString(), results);
-                this.attachment.push(attach);
+                // this.attachment.push(attach);
                 return;
             }
             return;
         }
+            ++this.passedTests;
 
-        if (results.passed && !this.options.notifyOnlyOnFailure === true) {
-            ++this.passedTests;
-            this.attachment.push(passedAttachment(test, results));
-        }else{
-            ++this.passedTests;
-        }
     }
 
     async after() {
         this.attachment[0].title = `${this.testNameFull}`;
         this.attachment[0].color = `#ffc107`;
         const req = this.attachment.push({author_name: `Total tests: ${this.tests} | Total passed: ${this.passedTests} | Total failed: ${this.failedTests}`, color: `#4366c7` });
-        if (this.failedTests > 0 && this.options.notifyOnlyOnFailure === true) {
+
             await this.webhook.send({ attachments: this.attachment });
             return;
-        }
-        if(!this.options.notifyOnlyOnFailure === true) {
-            await this.webhook.send({ attachments: this.attachment });
-        }
+
       }
 
-    async onComplete(){
-        if(this.options.uploadFile === true){
-            const zip = new AdmZip();
-            const outputFile = "./allure-results/test.zip";
-            zip.addLocalFolder("./allure-results");
-            zip.writeZip(outputFile);
-            console.log(`Created ${outputFile} successfully`);
 
-            // WebClient instantiates a client that can call API methods
-            // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-            const client = new WebClient(this.options.botToken, {
-                // LogLevel can be imported and used to make debugging simpler
-                logLevel: LogLevel.DEBUG
-            });
-            const name = fs.readdirSync(this.options.pathFile);
-            // The name of the file you're going to upload
-            // const fileName = this.options.pathFile + name[0];
-            const fileName = './allure-results/test.zip';
-            // ID of channel that you want to upload file to
-            const channelId = this.options.channelId;
-            try {
-            // Call the files.upload method using the WebClient
-            const result = await client.files.upload({
-                // channels can be a list of one to many strings
-                channels: channelId,
-                initial_comment: "Here\'s test result file :smile:",
-                // Include your filename in a ReadStream here
-                file: fs.createReadStream(fileName)
-            });
-                // console.log(result);
-            }
-            catch (error) {
-                console.error(error);
-            }
-        }
-    }
 }
 
 module.exports = SlackService;
